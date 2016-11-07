e2deb21eb4777bd7a9a8f7cbdf7782fbe03a5e0a
Paweł Luszuk <luszukpawel@gmail.com>
2016-11-07 11:34:11 +0100

    100% rspec coverage + additional tests

--- lib/heap.rb
+++ lib/heap.rb
@@ -1,8 +1,10 @@
-#def main
+# => Implementation of Heapsort alghorithm in Ruby plus some useful array methods
+
+# => def main
 #  a = [3, 2, 1]
 #  puts heapsort(a)
 #  puts extract_max(a)
-#end
+# => end
 
 def heapsort(input_array)
   heap_size = input_array.size
@@ -25,11 +27,20 @@ def heapsort_dec(input_array)
   heapsort(input_array).reverse
 end
 
-# def test(input_array, largest, index, heap_size)
-  # return unless largest != index
-  # input_array[index], input_array[largest] = input_array[largest], input_array[index]
-  # maxheapify(input_array, heap_size, index)
-# end
+def maxheapify(input_array, heap_size, index)
+  left = (index + 1) * 2 - 1
+  right = (index + 1) * 2
+  largest = if left < heap_size && input_array[left] > input_array[index]
+              left
+            elsif right < heap_size && input_array[right] > input_array[index]
+              right
+            else
+              index
+            end
+  return unless largest != index
+  input_array[index], input_array[largest] = input_array[largest], input_array[index]
+  maxheapify(input_array, heap_size, index)
+end
 
 def getmaxvalue(input_array)
   maximum = input_array[0]
@@ -44,11 +55,6 @@ def removefromlist(input_array, x)
   input_array
 end
 
-def addtolist(input_array)
-  input_array.add(x)
-  input_array
-end
-
 def get_size(input_array)
   x = input_array.size
   x
@@ -58,20 +64,4 @@ def empty?(input_array)
   return true if get_size(input_array).zero?
   false
 end
-
-def maxheapify(input_array, heap_size, index)
-  left = (index + 1) * 2 - 1
-  right = (index + 1) * 2
-  largest = index
-  largest = if left < heap_size && input_array[left] > input_array[index]
-              left
-            elsif right < heap_size && input_array[right] > input_array[largest]
-              right
-            else
-              index
-            end
-  return unless largest != index
-  input_array[index], input_array[largest] = input_array[largest], input_array[index]
-  maxheapify(input_array, heap_size, index)
-end
-#main
+# main
--- spec/heap_spec.rb
+++ spec/heap_spec.rb
@@ -1,89 +1,89 @@
-
 require 'simplecov'
 SimpleCov.start
 
 require 'heap'
 
-
 RSpec.describe 'heap.rb' do
   describe '#heapsort' do
-  it '#heapsort is defined' do
-    expect { heapsort([3, 2, 1]) }.not_to raise_error
+    it '#heapsort is defined' do
+      expect { heapsort([3, 2, 1]) }.not_to raise_error
+    end
+
+    it 'sorting does not loses any data' do
+      expect(heapsort([3, 2, 1])).to contain_exactly(1, 3, 2)
+    end
+
+    it 'sorting correctly' do
+      #  expect { heapsort([3, 2, 1]) }.to eq([1, 2, 3])
+      expect(heapsort([3, 2, 1])).to eq([1, 2, 3])
+      expect(heapsort([10, 40, 3, 2, 1, 3])).to eq([1, 2, 3, 3, 10, 40])
+    end
+
+    it '#maxheapify is defined' do
+      expect { maxheapify([5, 100, 200, -7], 4, 1) }.not_to raise_error
+    end
+
+    it '#heapsort_dec is defined' do
+      expect { heapsort_dec([3, 2, 1]) }.not_to raise_error
+    end
+
+    it 'heapsort_dec does not loses any data' do
+      #  expect { heapsort([3, 2, 1]) }.to eq([1, 2, 3])
+      expect(heapsort_dec([3, 2, 1])).to contain_exactly(1, 3, 2)
+    end
+
+    it 'heapsort_dec sorting correctly' do
+      #  expect { heapsort([3, 2, 1]) }.to eq([1, 2, 3])
+      expect(heapsort_dec([3, 2, 1, 5])).to eq([5, 3, 2, 1])
+    end
+
+    it 'sorting working in strings' do
+      #  expect { heapsort([3, 2, 1]) }.to eq([1, 2, 3])
+      expect(heapsort(%w(lorem ipsum dolor sit amet))).to eq(%w(amet dolor ipsum lorem sit))
+    end
+
+    it 'getmaxvalue is defined' do
+      expect { getmaxvalue([5, 100, 200, -7]) }.not_to raise_error
+    end
+
+    it 'getmaxvalue of int is int ' do
+      expect { getmaxvalue([5, 100, 200, -7]).to be_a_kind_of(integer) }
+    end
+
+    it 'getmaxvalue is working correctly' do
+      expect(getmaxvalue([5, 100, 200])).to eq(200)
+    end
+
+    it 'removefromlist is defined' do
+      expect { removefromlist([5, 100, 200, -7], 2) }.not_to raise_error
+    end
+
+    it 'removefromlist is working correctly' do
+      expect(removefromlist([5, 100, 200, -7], 200)).to eq([5, 100, -7])
+    end
+
+    it 'empty? is defined' do
+      expect { empty?([5, 10]) }.not_to raise_error
+    end
+
+    it 'empty? is returning correct type' do
+      expect { empty?([5, 100, 200, -7]).to be_a_kind_of(Boolean) }
+    end
+
+    it 'empty? is working correctly' do
+      expect(empty?([5, 10])).to eq(false)
+    end
+
+    it 'get_size is defined' do
+      expect { get_size([5, 10]) }.not_to raise_error
+    end
+
+    it 'get_size is working correctly' do
+      expect(get_size([5, 10])).to equal(2)
+    end
+
+    it 'get_size is returning correct type ' do
+      expect { getmaxvalue([5, 100, 200, -7]).to be_a_kind_of(integer) }
+    end
   end
-end
-
-  it 'sorting does not loses any data' do
-    #  expect { heapsort([3, 2, 1]) }.to eq([1, 2, 3])
-    expect(heapsort([3, 2, 1])).to contain_exactly(1, 3, 2)
-  end
-
-  it 'sorting correctly' do
-    #  expect { heapsort([3, 2, 1]) }.to eq([1, 2, 3])
-    expect(heapsort([3, 2, 1])).to eq([1, 2, 3])
-  end
-
-  it '#maxheapify is defined' do
-    expect { maxheapify([5, 100, 200, -7], 4, 1) }.not_to raise_error
-  end
-
-  it '#maxheapify is working correctly' do
-    #todo
-  end
-
-  it '#heapsort_dec is defined' do
-    expect { heapsort_dec([3, 2, 1]) }.not_to raise_error
-  end
-
-  it 'heapsort_dec does not loses any data' do
-    #  expect { heapsort([3, 2, 1]) }.to eq([1, 2, 3])
-    expect(heapsort_dec([3, 2, 1])).to contain_exactly(1, 3, 2)
-  end
-
-  it 'heapsort_dec sorting correctly' do
-    #  expect { heapsort([3, 2, 1]) }.to eq([1, 2, 3])
-    expect(heapsort_dec([3, 2, 1, 5])).to eq([5, 3, 2, 1])
-  end
-
-  it 'abc ?' do
-    #  expect { heapsort([3, 2, 1]) }.to eq([1, 2, 3])
-    expect(heapsort(["b", 'c', 'g'])).to contain_exactly("b", 'c', 'g')
-  end
-
-  it '#getmaxvalue is defined' do
-    expect { getmaxvalue([5, 100, 200, -7]) }.not_to raise_error
-  end
-
-  it 'getmaxvalue is working correctly'  do
-  expect(getmaxvalue( [5, 100, 200])).to eq(200)
-end
-
-  it '#removefromlist is defined' do
-    expect { removefromlist([5, 100, 200, -7], 2) }.not_to raise_error
-  end
-
-  it 'removefromlist is working correctly' do
-    expect(removefromlist([5, 100, 200, -7], 200) ).to eq([5, 100, -7])
-  end
-
-  it 'addtolist is defined' do
-
-  end
-
-  it 'addtolist is is working correctly' do
-  end
-
-
-  it 'empty? is defined' do
-  end
-
-  it 'empty? is working correctly' do
-  end
-
-  it 'get_size is defined' do
-  end
-
-  it 'get_size is working correctly' do
-  end
-
-
 end
